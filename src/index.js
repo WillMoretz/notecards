c566a06a48bb621e514b9251c42fc8a909c74a82
Will Moretz <whmoretz@gmail.com>
2023-02-23 21:32:35 -0500

    wrote pseudocode

---
+++ src/index.js
@@ -0,0 +1,28 @@
+// Pseudo code (some of these code blocks may later be moved to become their own modules)
+
+// notecardStorage Module Pattern
+// will store notecards in an array. Will add and delete notecards
+
+// notecard factory function
+// returns a new notecard
+
+// localstorage (probably should be its own module)
+// function that stores a notecard object and a function that returns a notecard object out of storage
+
+// subjectStorage Module Pattern
+// will store subjects. Will add and delete subjects
+
+// subject factory function
+// returns a new subject
+// will have a function the returns all notecards classified under the subject
+
+// tagStorage Module Pattern
+// will store tags. Will add and delete tags
+// will have a function that returns all notecards that have the supplied tags
+
+// DOMstuff: will be in its own module
+
+// pageManager module pattern
+// will call DOM functions and functions defined above
+
+// Event Listeners that will interface with pageManager
