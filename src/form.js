5f4570ceb5ef87a2cde4869774ad783068d2f85a
Will Moretz <whmoretz@gmail.com>
2023-03-01 21:46:02 -0500

    created subject form generation DOM

--- src/form.js
+++ src/form.js
@@ -1,11 +1,49 @@
-const form = (() => {
-  function generateForm() {
+const formDOM = (() => {
+  function generateNotecardForm() {
     const form = document.createElement("form");
     form.textContent = "hello form";
     return form;
   }
 
-  return { generateForm };
+  function generateSubjectForm() {
+    const form = document.createElement("form");
+    form.classList.add("pop-up");
+
+    const formRow1 = document.createElement("div");
+    formRow1.classList.add("form-row");
+
+    const formItem = document.createElement("div");
+    formItem.classList.add("form-item");
+
+    const subjectLabel = document.createElement("label");
+    subjectLabel.setAttribute("for", "subject");
+    subjectLabel.textContent = "Subject";
+    formItem.appendChild(subjectLabel);
+
+    const subjectInput = document.createElement("input");
+    subjectInput.setAttribute("type", "text");
+    subjectInput.setAttribute("id", "subject");
+    subjectInput.setAttribute("name", "subject");
+    formItem.appendChild(subjectInput);
+    formRow1.appendChild(formItem);
+    form.appendChild(formRow1);
+
+    const formRow2 = document.createElement("div");
+    formRow2.classList.add("form-row");
+
+    const submitButton = document.createElement("button");
+    submitButton.setAttribute("type", "submit");
+    submitButton.textContent = "submit";
+    submitButton.addEventListener("click", (e) => {
+      e.preventDefault();
+    });
+    formRow2.appendChild(submitButton);
+    form.appendChild(formRow2);
+
+    return form;
+  }
+
+  return { generateNotecardForm, generateSubjectForm };
 })();
 
-export default form;
+export default formDOM;
