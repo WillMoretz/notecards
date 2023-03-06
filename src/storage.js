0ca2e862e2ca829fed40513585b4013e7f4ada39
Will Moretz <whmoretz@gmail.com>
2023-03-05 21:30:19 -0500

    added tag and subject storing and restoring

--- src/storage.js
+++ src/storage.js
@@ -8,12 +8,21 @@ import {
 const storer = (() => {
   function storeNotecards() {}
 
-  function storeSubjects() {}
+  function storeSubjects(subjectList) {
+    for (let i = 0; i < subjectList.length; i += 1) {
+      localStorage.setItem(`subject${i}`, subjectList[i]);
+    }
+  }
 
-  function storeTags() {}
+  function storeTags(tagList) {
+    for (let i = 0; i < tagList.length; i += 1) {
+      localStorage.setItem(`tag${i}`, tagList[i]);
+    }
+  }
 
-  function store() {
-    console.log("store function ran");
+  function store(tagList, subjectList) {
+    storeTags(tagList);
+    storeSubjects(subjectList);
   }
 
   return { store };
@@ -22,9 +31,31 @@ const storer = (() => {
 const restorer = (() => {
   function restoreNotecards() {}
 
-  function restoreSubjects() {}
+  function restoreSubjects() {
+    const subjectList = [];
+    let i = 0;
+    while (true) {
+      const item = localStorage.getItem(`subject${i}`);
+      if (item !== null) {
+        subjectList.push(item);
+        i += 1;
+      } else break;
+    }
+    return subjectList;
+  }
 
-  function restoreTags() {}
+  function restoreTags() {
+    const tagList = [];
+    let i = 0;
+    while (true) {
+      const item = localStorage.getItem(`tag${i}`);
+      if (item !== null) {
+        tagList.push(item);
+        i += 1;
+      } else break;
+    }
+    return tagList;
+  }
 
   function restore() {
     console.log("restore function ran");
